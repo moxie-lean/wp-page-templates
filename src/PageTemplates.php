51cde21694ae9cd63dc365fecd9b204f8f62aeab
mitogh <mitogh@gmail.com>
2016-04-24 18:33:44 -0500

    Fix CI issues

--- src/PageTemplates.php
+++ src/PageTemplates.php
@@ -14,14 +14,14 @@ class PageTemplates {
 		// Create the key used for the themes cache.
 		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
 
-		// Retrieve the cache list. If it doesn't exist, or it's empty prepare an array
+		// Retrieve the cache list. If it doesn't exist, or it's empty prepare an array.
 		$templates = wp_cache_get( $cache_key, 'themes' );
 		if ( empty( $templates ) ) {
 			$templates = array();
 		}
 
-		// Since we've updated the cache, we need to delete the old cache
-		wp_cache_delete( $cache_key , 'themes');
+		// Since we've updated the cache, we need to delete the old cache.
+		wp_cache_delete( $cache_key , 'themes' );
 
 		// Now add our template to the list of templates by merging our templates
 		// with the existing templates array from the cache.
@@ -31,7 +31,7 @@ class PageTemplates {
 		);
 
 		// Add the modified cache to allow WordPress to pick it up for listing
-		// available templates
+		// available templates.
 		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
 	}
 }
